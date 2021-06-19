3a9bb31e9d6278a66b2968e2a2eb685855e15fd0
Min Soe Aung <minsoeaung2001@gmail.com>
2021-06-19 12:00:46 +0630

    Basic Authentication

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const url = 'mongodb://localhost:27017/conFusion'
 const connect = mongoose.connect(url)
 
 connect.then(() => {
-  console.log('Connected correctly to the server')
+	console.log('Connected correctly to the server')
 }, (err) => { console.log(err) })
 
 // view engine setup
@@ -31,7 +31,34 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+function auth(req, res, next) {
+	console.log(req.headers)
+	var authHeader = req.headers.authorization
+	if (!authHeader) {
+		var err = new Error('You are not authenticated!')
+		res.setHeader('WWW-Authenticate', 'Basic')
+		err.status = 401
+		next(err)
+		return
+	}
+
+	var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+	var user = auth[0];
+	var pass = auth[1];
+	if (user == 'admin' && pass == 'password') {
+		next(); // authorized
+	} else {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		next(err);
+	}
+}
+
+app.use(auth)
+
+app.use(express.static(path.join(__dirname, 'public')));  // to serve static pages from public folder
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -40,19 +67,19 @@ app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+	next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render('error');
 });
 
 module.exports = app;
