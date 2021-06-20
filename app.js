ba75d3c834c1ab402d7b5ea41f1df056edc19f6a
Min Soe Aung <minsoeaung2001@gmail.com>
2021-06-20 13:18:48 +0630

    Passport

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var FileStore = require('session-file-store')(session)
 var createError = require('http-errors')
 var path = require('path')
 var logger = require('morgan')
+var passport = require('passport')
+var authenticate = require('./authenticate')
 
 var indexRouter = require('./routes/index')
 var dishRouter = require('./routes/dishRouter')
@@ -38,26 +40,23 @@ app.use(session({
 	store: new FileStore()
 }))
 
+
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use('/', indexRouter)
 app.use('/users', usersRouter)
 
 function auth(req, res, next) {
-	console.log(req.session)
+	console.log(req.user);
 
-	if (!req.session.user) {
-		var err = new Error('You are not authenticated!')
-		res.setHeader('WWW-Authenticate', 'Basic')
-		err.status = 403
-		return next(err)
-	} else {
-		if (req.session.user === 'authenticated') {
-			next()
-		}
-		else {
-			var err = new Error('You are not authenticated!')
-			err.status = 403
-			return next(err)
-		}
+	if (!req.user) {
+		var err = new Error('You are not authenticated!');
+		err.status = 403;
+		next(err);
+	}
+	else {
+		next();
 	}
 }
 
