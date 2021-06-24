7bf6d331228b145bee479d37bf15e69fbe5f3792
Min Soe Aung <minsoeaung2001@gmail.com>
2021-06-24 12:35:06 +0630

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-12345-67890',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '964874801003874',
+        clientSecret: '840b40e8ae97ceb2298d861a5ece4828'
+    }
 }
