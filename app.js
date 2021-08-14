8ae497eb7a5fc67b46ad0993f550b537c122fbc3
minsoeaung <minsoeaung2001@gmail.com>
2021-08-14 20:20:14 +0630

    added /comments route

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const leaderRouter = require('./routes/leaderRouter');
 const usersRouter = require('./routes/users');
 const uploadRouter = require('./routes/uploadRouter')
 const favoriteRouter = require('./routes/favoritesRouter');
+const commentRouter = require('./routes/commentRouter')
 
 const app = express();
 
@@ -61,6 +62,7 @@ app.use('/promotions', promoRouter)
 app.use('/leaders', leaderRouter)
 app.use('/imageUpload', uploadRouter)
 app.use('/favorites', favoriteRouter)
+app.use('/comments', commentRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- models/dishes.js
+++ models/dishes.js
@@ -3,24 +3,7 @@ require('mongoose-currency').loadType(mongoose)
 const Currency = mongoose.Types.Currency
 const Schema = mongoose.Schema
 
-const commentSchema = new Schema({
-    rating: {
-        type: Number,
-        min: 1,
-        max: 5,
-        required: true
-    },
-    comment: {
-        type: String,
-        required: true
-    },
-    author: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-    }
-}, {
-    timestamps: true
-})
+
 
 const dishSchema = new Schema({
     name: {
@@ -52,8 +35,7 @@ const dishSchema = new Schema({
     featured: {
         type: Boolean,
         default:false      
-    },
-    comments: [ commentSchema ]
+    }
 }, {
     timestamps: true
 })
