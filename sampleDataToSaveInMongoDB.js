91f9447ec9e7500ea073e64dde6fb4d66c60217c
minsoeaung <minsoeaung2001@gmail.com>
2021-11-02 13:17:10 +0630

    Sample data for mongodb

---
+++ sampleDataToSaveInMongoDB.js
@@ -0,0 +1,68 @@
+dishes = [{
+    "label": "",
+    "featured": true,
+    "name": "Zucchipakoda",
+    "image": "images/zucchipakoda.png",
+    "category": "appetizer",
+    "price": 199,
+    "description": "Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce"
+}, {
+    "label": "New",
+    "featured": false,
+    "name": "Vadonut",
+    "image": "images/vadonut.png",
+    "category": "appetizer",
+    "price": 199,
+    "description": "A quintessential ConFusion experience, is it a vada or is it a donut?"
+}, {
+    "label": "Hot",
+    "featured": true,
+    "name": "Uthappizza",
+    "image": "images/uthappizza.png",
+    "category": "mains",
+    "price": 499,
+    "description": "A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer."
+}, {
+    "label": "",
+    "featured": false,
+    "name": "ElaiCheese Cake",
+    "image": "images/elaicheesecake.png",
+    "category": "dessert",
+    "price": 299,
+    "description": "A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms"
+}]
+
+leaders = [{
+    "featured": false,
+    "name": "Peter Pan",
+    "image": "images/alberto.png",
+    "designation": "Chief Epicurious Officer",
+    "description": "Our CEO, Peter, credits his hardworking East Asian immigrant parents who undertook the arduous journey to the shores of America with the intention of giving their children the best future. His mother's wizardy in the kitchen whipping up the tastiest dishes with whatever is available inexpensively at the supermarket, was his first inspiration to create the fusion cuisines for which The Frying Pan became well known. He brings his zeal for fusion cuisines to this restaurant, pioneering cross-cultural culinary connections."
+}, {
+    "featured": false,
+    "name": "Dhanasekaran Witherspoon",
+    "image": "images/alberto.png",
+    "designation": "Chief Food Officer",
+    "description": "Our CFO, Danny, as he is affectionately referred to by his colleagues, comes from a long established family tradition in farming and produce. His experiences growing up on a farm in the Australian outback gave him great appreciation for varieties of food sources. As he puts it in his own words, Everything that runs, wins, and everything that stays, pays!"
+}, {
+    "featured": false,
+    "name": "Agumbe Tang",
+    "image": "images/alberto.png",
+    "designation": "Chief Taste Officer",
+    "description": "Blessed with the most discerning gustatory sense, Agumbe, our CFO, personally ensures that every dish that we serve meets his exacting tastes. Our chefs dread the tongue lashing that ensues if their dish does not meet his exacting standards. He lives by his motto, You click only if you survive my lick."
+}, {
+    "featured": true,
+    "name": "Alberto Somayya",
+    "image": "images/alberto.png",
+    "designation": "Executive Chef",
+    "description": "Award winning three-star Michelin chef with wide International experience having worked closely with whos-who in the culinary world, he specializes in creating mouthwatering Indo-Italian fusion experiences. He says, Put together the cuisines from the two craziest cultures, and you get a winning hit! Amma Mia!"
+}]
+
+promotions = [{
+    "label": "New",
+    "featured": true,
+    "name": "Weekend Grand Buffet",
+    "image": "images/buffet.png",
+    "price": 19,
+    "description": "Featuring mouthwatering combinations with a choice of five different salads, six enticing appetizers, six main entrees and five choicest desserts. Free flowing bubbly and soft drinks. All for just $19.99 per person "
+}]
